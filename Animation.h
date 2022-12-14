9ad9d3ccaba201d60f8da790d0ff2ca9828a4b9c
C-Thanh <116700401+C-Thanh@users.noreply.github.com>
2022-12-14 16:23:28 +0700

    !!!

--- Animation.h
+++ Animation.h
@@ -24,7 +24,8 @@ public:
 };
 const Animation carAnimatorRight(carAnim, 100, COLOUR::FG_DARK_MAGENTA);
 const Animation carAnimatorLeft(carAnim1, 100, COLOUR::FG_CYAN);
-const Animation manAnimator(manAnim, 300, COLOUR::FG_DARK_GREY);
+const Animation manAnimatorLeft(manAnimLeft, 300, COLOUR::FG_DARK_GREY);
+const Animation manAnimatorRight(manAnimRight, 300, COLOUR::FG_DARK_GREY);
 const Animation truckAnimatorLeft(truckAnimLeft, 300, COLOUR::FG_RED);
 const Animation truckAnimatorRight(truckAnimRight, 300, COLOUR::FG_DARK_BLUE);
 const Animation deerAnimatorLeft(deerAnim_left, 300, COLOUR::FG_DARK_MAGENTA);
@@ -33,5 +34,6 @@ const Animation duckAnimatorLeft(duckAnimLeft, 300, COLOUR::FG_YELLOW);
 const Animation duckAnimatorRight(duckAnimRight, 300, COLOUR::FG_YELLOW);
 const Animation dancingAnimator(dancing, 300, COLOUR::FG_BLACK);
 const Animation introAnimator(introAnim, 300, COLOUR::FG_DARK_BLUE);
-const Animation mainChar(mainAnim, 200, COLOUR::FG_BLACK);
+const Animation mainCharLeft(mainAnimLeft, 200, COLOUR::FG_BLACK);
+const Animation mainCharRight(mainAnimRight, 200, COLOUR::FG_BLACK);
 #endif // !_ANIMATION_
